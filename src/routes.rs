f0ea94adbb4f675a5cc50096ad766a38e73daffb
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-10-29 11:29:40 -0400

    Add main chat_server route

--- src/routes.rs
+++ src/routes.rs
@@ -18,3 +18,23 @@ type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 pub async fn index() -> impl Responder {
     NamedFile::open_async("./static/index.html").await.unwrap()
 }
+
+pub async fn chat_server(
+    req: HttpRequest,
+    stream: web::Payload,
+    pool: web::Data<DbPool>,
+    srv: web::Data<Addr<server::ChatServer>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        session::WsChatSession {
+            id: 0,
+            hb: Instant::now(),
+            room: "main".to_string(),
+            name: None,
+            addr: srv.get_ref().clone(),
+            db_pool: pool,
+        },
+        &req,
+        stream
+    )
+}
