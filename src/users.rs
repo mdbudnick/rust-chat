fec6119d96d1d416b052c8cfb54763410f0ba66d
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-10-29 11:32:53 -0400

    Create and Get users.rs:

---
+++ src/users.rs
@@ -0,0 +1,39 @@
+#[post("/users/create")]
+pub async fn create_user(
+    pool: web::Data<DbPool>,
+    form: web::Json<models::NewUser>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let mut conn = pool.get()?;
+        db::insert_new_user(&mut conn, &form.username, &form.phone)
+    })
+    .await?
+    .map_err(actix_web::error::ErrorUnprocessableEntity)?;
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[get("/users/{user_id}")]
+pub async fn get_user_by_id(
+    pool: web::Data<DbPool>,
+    id: web::Path<Uuid>,
+) -> Result<HttpResponse, Error> {
+    let user_id = id.to_owned();
+    let user = web::block(move || {
+        let mut conn = pool.get()?;
+        db::find_user_by_uid(&mut conn, user_id)
+    })
+    .await?
+    .map_err(actix_web::error::ErrorInternalServerError)?;
+    if let Some(user) = user {
+        Ok(HttpResponse::Ok().json(user))
+    } else {
+        let res = HttpResponse::NotFound().body(
+            json!({
+                "error": 404,
+                "message": format!("No user found with phone: {id}")
+            })
+            .to_string(),
+        );
+        Ok(res)
+    }
+}
