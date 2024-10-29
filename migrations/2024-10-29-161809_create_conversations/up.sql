11949bb829e9c86a197b4839bd0227d7cbd5c13e
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-10-29 12:27:59 -0400

    Initialize dev db

--- migrations/2024-10-29-161809_create_conversations/up.sql
+++ migrations/2024-10-29-161809_create_conversations/up.sql
@@ -4,4 +4,19 @@ CREATE TABLE conversations (
   user_id TEXT NOT NULL,
   content VARCHAR NOT NULL,
   created_at TEXT NOT NULL
-)
+);
+
+CREATE TABLE rooms (
+  id TEXT PRIMARY KEY NOT NULL,
+  name VARCHAR NOT NULL,
+  last_message TEXT NOT NULL,
+  participant_ids TEXT NOT NULL,
+  created_at TEXT NOT NULL
+);
+
+CREATE TABLE users (
+  id TEXT PRIMARY KEY NOT NULL,
+  username VARCHAR NOT NULL,
+  created_at TEXT NOT NULL,
+  unique(username)
+);
