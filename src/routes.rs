1c3844c4181c1b419bef7d0bff16a8d29cc919a7
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-10-29 15:46:32 -0400

    Remove unused imports

--- src/routes.rs
+++ src/routes.rs
@@ -1,16 +1,12 @@
 use std::time::Instant;
 use actix::*;
 use actix_files::NamedFile;
-use actix_web::{get, post, web, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::{web, Error, HttpRequest, HttpResponse, Responder};
 use actix_web_actors::ws;
 use diesel::{
     prelude::*,
     r2d2::{self, ConnectionManager},
 };
-use serde_json::json;
-use uuid::Uuid;
-use crate::db;
-use crate::models;
 use crate::server;
 use crate::session;
 
